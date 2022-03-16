af4e39b89dbb803df4fd4472548f596d75756faa
mack edwards <mackedwards@macks-MacBook-Air.local>
2022-03-16 12:01:47 -0600

    added handleFunctions

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,17 +1,30 @@
 import {useState, useEffect} from 'react'
 
 const Add = (props) => {
+  // handling state
   const emptyItem = {name: '', category: '', cost: ''}
   const [item, setItem] = useState(emptyItem)
+
+  // function to handle the changes for our object
+  const handleChange = (event) => {
+    //  this line below is probably going to break ....
+    setItem({...item, [event.target.name]: event.target.value})
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    props.handleCreate(item)
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name: </label>
-        <input type="text" name="name" />
+        <input type="text" name="name" value={item.name} onChange={handleChange}/>
         <label htmlFor="category">Category: </label>
-        <input type="text" name="category" />
+        <input type="text" name="category" value={item.category} onChange={handleChange}/>
         <label htmlFor="cost">Cost: </label>
-        <input type="number" name="cost" />
+        <input type="number" name="cost" value={item.cost} onChange={handleChange}/>
         <input type="submit" />
       </form>
     </>
